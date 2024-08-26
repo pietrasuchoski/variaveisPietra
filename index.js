9714d08762bb4146a9bd6d288a54f20f337a321f
pietrasuchoski <167487724+pietrasuchoski@users.noreply.github.com>
2024-08-26 23:03:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+let idade = 28;
+const anoNascimento = 1995;
+let anoAtual = 2024
+const nomeCompleto= "Lucas Stasiak";
+
+console.log("Meu nome é "  + nomeCompleto + " nascido no ano de" + anoNascimento + " e no ano atual" + anoAtual + " completo" + idade + " anos");
+
+anoAtual = anoAtual + 1;
+console.log(anoAtual);
+
+idade = idade + 1;
+console.log(idade);
+
+
+//Agencia e viagens Hawaii Paris Nova York Rio de Janeiro Tokyo
+const listaDeDestinos = new Array(
+  "Hawaii",
+  "Paris",
+  "Nova York",
+  "Rio de Janeiro",
+  "Toquio"
+);
+
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[3]);
+listaDeDestinos.push( "Coronel Vivida");
+listaDeDestinos.push("Curitiba");
+listaDeDestinos.push("Rio Branco");
+listaDeDestinos.push("Berlin");
+console.log(listaDeDestinos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
